781a67857a90a4bee73118e9752eefec99e22a6b
AngelBarrios <angelefrenbarriosromero234@gmail.com>
2023-10-18 00:14:45 -0300

    itemDetail

--- src/components/pages/itemListContainer/ItemListContainer.jsx
+++ src/components/pages/itemListContainer/ItemListContainer.jsx
@@ -2,14 +2,27 @@ import "./ItemListContainer.css";
 import { useState } from "react";
 import { productos } from "../../../productsMock";
 import { useParams } from "react-router-dom";
+import ItemList from "./ItemList";
 
-const ItemListContainer = (props) => {
-  return (
-    <div className="container">
-      <h2>{props.titulo}</h2>
-      <p>Todos los juguetes que te puedas imaginar y más...</p>
-    </div>
-  );
+const ItemListContainer = () => {
+  const [items, setItems] = useState([]);
+
+  const { categoryName } = useParams();
+  console.log(categoryName ? "estoy intentando filtrar" : "Estoy en el home");
+
+  useEffect(() => {
+    const productosFiltrados = products.filter(
+      (product) => product.category === categoryName
+    );
+
+    const tarea = new Promise((resolve, reject) => {
+      resolve(categoryName ? productosFiltrados : products);
+    });
+
+    tarea.then((res) => setItems(res)).catch((error) => console.log(error));
+  }, [categoryName]);
+
+  return <ItemList items={items} />;
 };
 
 export default ItemListContainer;
